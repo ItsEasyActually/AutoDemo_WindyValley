38ce2779e916f0b63906a5c3803995c2612553d8
supercoolsonic <joeyj95@hotmail.com>
2018-06-04 18:35:34 -0500

    Adding new Dyn_Col loads

--- AutoDemo_WindyValley/Pedestal_Functions.cpp
+++ AutoDemo_WindyValley/Pedestal_Functions.cpp
@@ -7,7 +7,10 @@
 
 void __cdecl Load_IDai1(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai1;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -32,12 +35,15 @@ void __cdecl Load_IDai1(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai2(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai2;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -62,12 +68,15 @@ void __cdecl Load_IDai2(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai3(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai3;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -92,12 +101,15 @@ void __cdecl Load_IDai3(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai4(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai4;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -122,12 +134,15 @@ void __cdecl Load_IDai4(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai5(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai5;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -152,12 +167,15 @@ void __cdecl Load_IDai5(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai6(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai6;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -182,12 +200,15 @@ void __cdecl Load_IDai6(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai7(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai7;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -212,12 +233,15 @@ void __cdecl Load_IDai7(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai8(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai8;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -242,12 +266,15 @@ void __cdecl Load_IDai8(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai9(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &I_Dai9;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -272,12 +299,15 @@ void __cdecl Load_IDai9(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
 
 void __cdecl Load_IDai10(ObjectMaster *a1)
 {
-	NJS_OBJECT *obj;
+	a1->Data1->Object = &Unreferenced01_00106B30;
+	SetStatus(a1);
+	DynCol_LoadObject(a1);
+	/*NJS_OBJECT *obj;
 	EntityData1 *v1;
 
 	v1 = a1->Data1;
@@ -302,5 +332,5 @@ void __cdecl Load_IDai10(ObjectMaster *a1)
 		a1->MainSub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DisplaySub = (void(__cdecl *)(ObjectMaster *))Basic_Display;
 		a1->DeleteSub = (void(__cdecl *)(ObjectMaster *))DeleteDynamicCollision;
-	}
+	}*/
 }
