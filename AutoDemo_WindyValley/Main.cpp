1ed0da2be6526f3d104933c973e4a967b6a9cdf8
supercoolsonic <joeyj95@hotmail.com>
2018-07-20 14:53:24 -0500

    Adjusting skybox pos...
    ...for Act 1 by a bit to match footage better.

--- AutoDemo_WindyValley/Main.cpp
+++ AutoDemo_WindyValley/Main.cpp
@@ -258,6 +258,7 @@ void __cdecl WVAct1_Display(ObjectMaster *a1)
 		sub_408530(&Act1MainSkybox);
 
 		njPushMatrix(0);
+		njTranslate(0, 0, (Act1CloudRing1.pos[1] + 80.0f), 0);
 		v5 = *(float*)&v1->CharIndex * 65536.0 * 0.002777777777777778;
 		if (v5)
 		{
@@ -267,7 +268,7 @@ void __cdecl WVAct1_Display(ObjectMaster *a1)
 		njPopMatrix(1u); //cloud ring
 
 		njPushMatrix(0);
-		njTranslate(0, 0, (Act1CloudLayer.pos[1] + 300.0f), 0);
+		njTranslate(0, 0, (Act1CloudLayer.pos[1] + 380.0f), 0);
 		sub_408530(&Act1CloudLayer);
 		njPopMatrix(1u); //cloud layer
 		njPopMatrix(1u); //main model
