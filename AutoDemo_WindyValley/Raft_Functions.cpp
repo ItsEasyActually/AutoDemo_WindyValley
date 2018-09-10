778289aa1c9f7259d2d8dc8237f65c1e980f9178
supercoolsonic <joeyj95@hotmail.com>
2018-09-10 17:06:26 -0500

    Clipset check and movement
    Just for now, until you implement eases.

--- AutoDemo_WindyValley/Raft_Functions.cpp
+++ AutoDemo_WindyValley/Raft_Functions.cpp
@@ -28,31 +28,33 @@ void Raft_Main(ObjectMaster *a1)
 	float speed = 0.3f; //based on what you said
 
 	char timer = a1->Data1->CharIndex;
-	if (timer == 0) {
-		uint8_t state = a1->Data1->NextAction;
+	if (!ClipSetObject(a1)) {
+		if (timer == 0) {
+			uint8_t state = a1->Data1->NextAction;
 
-		if (state == 0) {
-			if (pos < dest) a1->Data1->Object->pos[1] += speed;
-			else {
-				a1->Data1->Object->pos[1] = dest;
-				a1->Data1->NextAction = 1; //state
+			if (state == 0) {
+				if (pos < orig) a1->Data1->Object->pos[1] += speed;
+				else {
+					a1->Data1->Object->pos[1] = orig;
+					a1->Data1->NextAction = 1; //state
 
-				a1->Data1->CharIndex = 10; //timer
+					a1->Data1->CharIndex = 10; //timer
+				}
+
 			}
-
-		}
-		else {
-			if (pos > orig) a1->Data1->Object->pos[1] -= speed;
 			else {
-				a1->Data1->Object->pos[1] = orig;
-				a1->Data1->NextAction = 0; //state
+				if (pos > dest) a1->Data1->Object->pos[1] -= speed;
+				else {
+					a1->Data1->Object->pos[1] = dest;
+					a1->Data1->NextAction = 0; //state
 
-				a1->Data1->CharIndex = 10; //timer
+					a1->Data1->CharIndex = 10; //timer
+				}
 			}
 		}
+		else a1->Data1->CharIndex--;
+		Raft_Display(a1);
 	}
-	else a1->Data1->CharIndex--;
-	Raft_Display(a1);
 }
 
 void __cdecl Load_Raft(ObjectMaster *a1)
@@ -65,7 +67,7 @@ void __cdecl Load_Raft(ObjectMaster *a1)
 	}
 	AddToCollision(a1, 4); //set up a moving collision with scaling, replaces a1->Data1->Object with a pointer to itself
 	//a1->Data1->Scale.y = a1->Data1->Position.y + a1->Data1->Scale.y; //dest pos
-	a1->Data1->Scale.y = (a1->Data1->Position.y + 10.0f);
+	a1->Data1->Scale.y = (a1->Data1->Position.y - 10.0f);
 
 	a1->MainSub = &Raft_Main;
 	a1->DisplaySub = &Raft_Display;
