5604b3077a1def7ba134a5cae1b308d1404fe27c
Sora <sora.x@hotmail.fr>
2023-01-12 00:32:29 +0100

    level: Small cleanup

--- AutoDemo_WindyValley/level.cpp
+++ AutoDemo_WindyValley/level.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
-LandTableInfo* ADWVGeo[3] = { nullptr };
+static const char Max = 3;
+LandTableInfo* ADWVGeo[Max] = { nullptr };
 
 NJS_TEXNAME textures_windy1[42];
 NJS_TEXLIST texlist_windy1 = { arrayptrandlength(textures_windy1) };
@@ -11,32 +12,35 @@ NJS_TEXLIST texlist_windy2 = { arrayptrandlength(textures_windy2) };
 NJS_TEXNAME textures_windy3[55];
 NJS_TEXLIST texlist_windy3 = { arrayptrandlength(textures_windy3) };
 
-
 void initADWV_Landtables()
 {
 	PrintDebug("AD WV: Loading Landtables...\n");
+	LandTable* ADWVTemp[Max] = { nullptr };
 
-	for (Uint8 i = 0; i < LengthOfArray(ADWVGeo); ++i) {
+	for (Uint8 i = 0; i < LengthOfArray(ADWVGeo); ++i) 
+	{
 		std::string str = "landtable020" + std::to_string(i);
 		std::string fullPath = "System\\" + str +".sa1lvl";
 		ADWVGeo[i] = new LandTableInfo(helper.GetReplaceablePath(fullPath.c_str()));
+		ADWVTemp[i] = ADWVGeo[i]->getlandtable();
 	}
 
-	LandTable* ADWV0 = ADWVGeo[0]->getlandtable();
-	ADWV0->TexName = "WINDY01";
-	ADWV0->TexList = &texlist_windy1;
+	if (!ADWVTemp[0])
+		return;
 
-	LandTable* ADWV1 = ADWVGeo[1]->getlandtable();
-	ADWV1->TexName = "WINDY02";
-	ADWV1->TexList = &texlist_windy2;
+	ADWVTemp[0]->TexName = "WINDY01";
+	ADWVTemp[0]->TexList = &texlist_windy1;
 
-	LandTable* ADWV2 = ADWVGeo[2]->getlandtable();
-	ADWV2->TexName = "WINDY03";
-	ADWV2->TexList = &texlist_windy3;
+	ADWVTemp[1]->TexName = "WINDY02";
+	ADWVTemp[1]->TexList = &texlist_windy2;
 
-	GeoLists[8 * LevelIDs_WindyValley + 0] = ADWV0;
-	GeoLists[8 * LevelIDs_WindyValley + 1] = ADWV1;
-	GeoLists[8 * LevelIDs_WindyValley + 2] = ADWV2;
+	ADWVTemp[2]->TexName = "WINDY03";
+	ADWVTemp[2]->TexList = &texlist_windy3;
+
+	for (Uint8 i = 0; i < LengthOfArray(ADWVGeo); ++i)
+	{
+		GeoLists[8 * LevelIDs_WindyValley + i] = ADWVTemp[i];
+	}
 
 	PrintDebug("Done.\n");
 }
