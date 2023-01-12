91d83d1478c8b7af06c315a6b0503c6cf4db4e26
ItsEasyActually <darkspines35@gmail.com>
2023-01-12 11:48:58 -0500

    Built new starting project for refactoring. Added all base object files and corresponding header files.

---
+++ ad_windyvalley/stg02.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+extern "C"
+{
+	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
+	{
+		
+	}
+
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+}
